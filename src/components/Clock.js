5d60fe38c22b5d5892a6f81a59d433a979364736
Harjit Kainth <harjitkainth10@gmail.com>
2021-08-16 11:31:34 -0700

    added comments

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,28 +1,33 @@
 // Attribution to react docs. Component built using tutorial
-
+// import react 
 import React from 'react';
 
+// build Clock component as class
 class Clock extends React.Component {
-
+    // constructor function initalize this.state
     constructor(props){
         super(props);
         this.state = {date: new Date()};
     }
 
+    // react function to track time when component was added to DOM
     componentDidMount() {
         this.timerID = setInterval(
             () => this.tick(), 1000
         );
     }
 
+    // react function to track time when component was removed from DOMG
     componentWillUnmount() {
         clearInterval(this.timerID);
     }
 
+    // tick function using setState of new date
     tick() {
         this.setState({date: new Date()});
     }
 
+    // react render function, rendering component to DOM 
     render() {
         return (
             <div className='clock-container'>
